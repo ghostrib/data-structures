2273a0ba37ccb97ae9c158d996cb0d397bdaec65
ghostrib <sh4rkbot@gmail.com>
2018-11-07 16:05:49 -0600

    Adds implemented functional-shared stack and queue classes

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,34 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.enqueue = queueMethods.enqueue;
+  someInstance.dequeue = queueMethods.dequeue;
+  someInstance.size = queueMethods.size;
+  return someInstance;
 };
 
 var queueMethods = {};
 
 
+queueMethods.enqueue = function(value) {
+  var keys = Object.keys(this.storage);
+  var numbers = [];
+  keys.forEach(function(key){
+    numbers.push(Number(key));
+  });
+  this.storage[numbers.length] = value;
+};
+
+queueMethods.dequeue = function() {
+  var firstKey = this.storage['0'];
+  var keys = Object.keys(this.storage);
+  for (var i = 1; i < keys.length; i++) {
+    this.storage[i - 1] = this.storage[i];
+  }
+  delete this.storage[keys.length - 1];
+  return firstKey;
+};
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  var keys = Object.keys(this.storage);
+  var numbers = [];
+  keys.forEach(function(key){
+    numbers.push(Number(key));
+  });
+  this.storage[numbers.length] = value;
+};
 
+stackMethods.pop = function() {
+  var keys = Object.keys(this.storage);
+  var numbers = [];
+  keys.forEach(function(key){
+    numbers.push(Number(key));
+  });
+  var lastValue = this.storage[numbers.length - 1];
+  delete this.storage[numbers.length - 1];
+  return lastValue;
+};
+
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+};
