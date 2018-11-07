813aeb99eb78bed2a6e59edd582017df8c16542f
ghostrib <sh4rkbot@gmail.com>
2018-11-07 12:31:59 -0600

    Completed stack and queue for functional instance

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,27 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    var keys = Object.keys(storage);
+    var numbers = [];
+    keys.forEach(function(key){
+      numbers.push(Number(key));
+    });
+    storage[numbers.length] = value;
   };
 
   someInstance.dequeue = function() {
+    var firstKey = storage['0'];
+    var keys = Object.keys(storage);
+    for (var i = 1; i < keys.length; i++) {
+      storage[i - 1] = storage[i];
+      // console.log(storage)
+    }
+    delete storage[keys.length -1];
+    return firstKey;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,29 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var keys = Object.keys(storage);
+    var numbers = [];
+    keys.forEach(function(key){
+      numbers.push(Number(key));
+    });
+    storage[numbers.length] = value;
   };
 
   someInstance.pop = function() {
+    var keys = Object.keys(storage);
+    var numbers = [];
+    keys.forEach(function(key){
+      numbers.push(Number(key));
+    });
+
+
+    var lastValue = storage[numbers.length-1];
+    delete storage[numbers.length-1];
+    return lastValue;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
